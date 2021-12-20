7bb995850790c972fe4e2cd7fe7bfd333e2bd61b
Horia Radu <horia.radu@e-spres-oh.com>
2021-12-20 15:08:56 +0200

    2-2

--- src/2-1/index.ts
+++ src/2-1/index.ts
@@ -3,22 +3,29 @@ import { readFile } from "fs/promises";
 const main = async () => {
   try {
     const data = await readFile("./src/2-1/input.txt", "utf8");
+    // const data = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2";
 
     const { depth, position } = data.split("\n").reduce(
-      ({ depth, position }, line) => {
-        const [cmd, value] = line.split(" ");
+      ({ depth, position, aim }, line) => {
+        const [cmd, v] = line.split(" ");
+        const value = parseInt(v, 10);
+
         switch (cmd) {
           case "forward":
-            return { depth, position: position + parseInt(value, 10) };
+            return {
+              depth: depth + value * aim,
+              position: position + value,
+              aim,
+            };
           case "down":
-            return { depth: depth + parseInt(value, 10), position };
+            return { depth, position, aim: aim + value };
           case "up":
-            return { depth: depth - parseInt(value, 10), position };
+            return { depth, position, aim: aim - value };
           default:
-            return { depth, position };
+            return { depth, position, aim };
         }
       },
-      { depth: 0, position: 0 }
+      { depth: 0, position: 0, aim: 0 }
     );
 
     console.log(depth * position);
